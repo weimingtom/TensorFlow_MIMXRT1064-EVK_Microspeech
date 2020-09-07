21d425415679542bac0cc9f3d42a617854e374e1
Matthias Hertel <Matthias.Hertel@arm.com>
2020-09-07 15:45:14 +0200

    Initial Commit Microspeech /mh

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,48 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'microspeech.Release' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "fsl_device_registers.h"
+
+/*  ARM::CMSIS:RTOS2:Keil RTX5:Source:5.5.2 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+/*  Keil.ARM Compiler::Compiler:Event Recorder:DAP:1.4.0 */
+#define RTE_Compiler_EventRecorder
+          #define RTE_Compiler_EventRecorder_DAP
+/*  Keil.ARM Compiler::Compiler:I/O:STDERR:User:1.2.0 */
+#define RTE_Compiler_IO_STDERR          /* Compiler I/O: STDERR */
+          #define RTE_Compiler_IO_STDERR_User     /* Compiler I/O: STDERR User */
+/*  Keil.ARM Compiler::Compiler:I/O:STDIN:User:1.2.0 */
+#define RTE_Compiler_IO_STDIN           /* Compiler I/O: STDIN */
+          #define RTE_Compiler_IO_STDIN_User      /* Compiler I/O: STDIN User */
+/*  Keil.ARM Compiler::Compiler:I/O:STDOUT:User:1.2.0 */
+#define RTE_Compiler_IO_STDOUT          /* Compiler I/O: STDOUT */
+          #define RTE_Compiler_IO_STDOUT_User     /* Compiler I/O: STDOUT User */
+/*  Keil::CMSIS Driver:VIO:Board:MIMXRT1064-EVK:1.0.0 */
+#define RTE_VIO_BOARD
+        #define RTE_VIO_MIMXRT1064_EVK
+/*  NXP::Device:SDK Drivers:xip_board:2.0.0 */
+#define XIP_EXTERNAL_FLASH 1
+#define XIP_BOOT_HEADER_ENABLE 1
+/*  NXP::Device:SDK Drivers:xip_device:2.0.0 */
+#define XIP_EXTERNAL_FLASH 1
+#define XIP_BOOT_HEADER_ENABLE 1
+/*  NXP::Device:SDK Utilities:serial_manager_uart:1.0.0 */
+#define SERIAL_PORT_TYPE_UART 1
+
+
+#endif /* RTE_COMPONENTS_H */
