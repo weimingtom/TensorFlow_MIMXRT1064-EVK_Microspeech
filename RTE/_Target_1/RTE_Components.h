93668da1cfab0ea58c1650d615556061ad8c1d44
Matthias Hertel <Matthias.Hertel@arm.com>
2020-09-10 12:12:22 +0200

    Source changes

--- RTE/_Target_1/RTE_Components.h
+++ RTE/_Target_1/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'microspeech.Release' 
+ * Project: 'microspeech' 
  * Target:  'Target 1' 
  */
 
